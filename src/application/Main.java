f7225e54918e54288c63d7999677a03a71ca545c
riclovato <riic.lovato@gmail.com>
2023-03-09 21:06:29 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,24 +6,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	// Método start() é onde a aplicação JavaFX é iniciada
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Criando um objeto FXMLLoader para carregar a view FXML
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			// Carregando a view FXML
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			// Criando uma cena para a view carregada
+			Scene mainScene = new Scene(scrollPane);
+			// Setando a cena no palco principal
 			primaryStage.setScene(mainScene);
+			// Definindo o título do palco principal
 			primaryStage.setTitle("Sample JavaFX application");
+			// Exibindo o palco principal
 			primaryStage.show();
 		} catch (IOException e) {
+			// Se houver uma exceção, imprime a stack trace
 			e.printStackTrace();
 		}
 	}
 
+	// Método main() que é chamado quando a aplicação é iniciada
 	public static void main(String[] args) {
+		// Chama o método launch() da classe Application, iniciando a aplicação JavaFX
 		launch(args);
 	}
 }
